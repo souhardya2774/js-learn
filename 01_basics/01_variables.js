d5a3b5f25ded6531b72c01aa184bda9082cdc55b
souhardya2774 <141372850+souhardya2774@users.noreply.github.com>
2024-01-04 18:54:55 +0000

    Till Part 1: 00:44:00

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144533;
+let accountEmail = "souhardya@gmail.com";
+var accountPassword = "12345";
+accountCity = "Falakata";
+
+// accountId = 2;
+accountEmail = "sd@gmail.com";
+accountPassword = "212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity]);
+// 00:44:00
